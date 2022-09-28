fc2b3efe2fc19a6aa29ceb60c2fcbeac2ec1a7e6
xRegiMariPinna <110124921+xRegiMariPinna@users.noreply.github.com>
2022-09-28 15:01:05 -0300

    Projeto77

---
+++ style.js
@@ -0,0 +1,35 @@
+console.log("Meu Primeiro projeto no console");
+
+
+
+
+
+
+
+
+var firstName = "Ranbir";
+var firstName = "Kapoor";
+
+
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
+
+var a = 10;
+var b = 5;
+
+
+var add = a + b ;
+console.log(add);
+
+
+var sub = a - b ;
+console.log(sub);
+
+
+var multiple = a * b;
+console.log(multiple);
+
+
+var divide = a / b ;
+conso
